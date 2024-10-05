f23314464f53a72e310b6e733b90547aa2b8a7ec
Sazzadur Rahman <sazzadurrahman000@gmail.com>
2024-10-05 14:22:55 +0600

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="mx-auto max-w-5xl text-2xl mb-10">{children}</div>
       </body>
     </html>
   );
